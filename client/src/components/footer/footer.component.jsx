8068d4761906635696968afd96d30d4009167d3e
lx kish <urban.ktulhu@gmail.com>
2020-10-07 15:22:36 +1300

    style(forms): style footer for desktop layout

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -1,54 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './footer.styles.scss';
+import "./footer.styles.scss";
 
-import IconFacebook from '../../components/icons/icon-facebook.component';
-import IconLinkedIn from '../../components/icons/icon-linkedin.component';
-
-// const content = {
-//     doubleLinkPane: {
-//         className: 'footer__media',
-//         links: [
-//             {
-//                 to: '/',
-//                 className: 'footer__link',
-//                 icon: IconFacebook,
-//                 iconClassName: 'footer__icon color-white'
-//             },
-//             {
-//                 to: '/',
-//                 className: 'footer__link',
-//                 icon: IconLinkedIn,
-//                 iconClassName: 'footer__icon color-white'
-//             }
-//         ]
-//     }
-// };
+import IconFacebook from "../../components/icons/icon-facebook.component";
+import IconLinkedIn from "../../components/icons/icon-linkedin.component";
 
 const Footer = () => (
-    <footer className='footer'>
-        <div className='footer__content'>
-            <p className='footer__copyright'>
-                I'm In is a New Zealand registered Charitable Trust (CC123456). All Rights Reserved. Copyright &copy;
-                2020.
-            </p>
-            <div className='footer__media'>
-                <Link
-                    to={'/'}
-                    className='footer__link'
-                >
-                    <IconFacebook className='footer__icon color-white' />
-                </Link>
-                <Link
-                    to={'/'}
-                    className='footer__link'
-                >
-                    <IconLinkedIn className='footer__icon color-white' />
-                </Link>
-            </div>
-        </div>
-    </footer>
+  <footer className="footer">
+    <p className="footer__copyright">
+      I'm In is a New Zealand registered Charitable Trust (CC123456). All Rights
+      Reserved. Copyright &copy; 2020.
+    </p>
+    <div className="footer__media">
+      <Link to={"/"} className="footer__link">
+        <IconFacebook className="footer__icon color-white" />
+      </Link>
+      <Link to={"/"} className="footer__link">
+        <IconLinkedIn className="footer__icon color-white" />
+      </Link>
+    </div>
+  </footer>
 );
 
 export default Footer;
